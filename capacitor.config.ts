a5dd4c6a236838eeb7ecca246d6f36f3786bb353
Nafiz Chawdhury <98878310+NafizChaw@users.noreply.github.com>
2025-09-16 20:15:13 -0400

    android and ios native build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.optipath.app',
+  appName: 'OptuPath',
+  webDir: 'dist'
+};
+
+export default config;
